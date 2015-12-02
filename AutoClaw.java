1a915eb7290087eb616a69e8df8121e29ab3930b
Drew Zoghby <Drew Zoghby>
2015-12-01 23:13:36 -0600

    created a PID loop function to handling turning and driving to avoid recoding it slightly different

--- AutoClaw.java
+++ AutoClaw.java
@@ -12,9 +12,6 @@ public class AutoClaw extends LinearOpMode {
     //drive motors
     DcMotor leftMotor;
     DcMotor rightMotor;
-    //claw motors
-    DcMotor leftMClaw;
-    DcMotor rightMClaw;
     //release serovos
     Servo releaseServo1;
     Servo releaseServo2;
@@ -27,12 +24,6 @@ public class AutoClaw extends LinearOpMode {
         //assign hardware to objects
         leftMotor = hardwareMap.dcMotor.get("left_Motor");
         rightMotor = hardwareMap.dcMotor.get("right_Motor");
-
-        leftMClaw = hardwareMap.dcMotor.get("left_claw_motor");
-        rightMClaw = hardwareMap.dcMotor.get("right_claw_motor");
-
-        releaseServo1 = hardwareMap.servo.get("rs1");
-        releaseServo2 = hardwareMap.servo.get("rs2");
         //assigning the hardware gyro to a variable
         gyro = hardwareMap.gyroSensor.get("gy");
 
@@ -109,5 +100,75 @@ public class AutoClaw extends LinearOpMode {
         }
 
     }
-
+    public double PID(char z, double t){
+        //reversing because its on oppisite side
+        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        //defining variables
+        double kp;
+        double ki;
+        double kd;
+        double error;
+        double previous_error;
+        double proportional;
+        double intergral;
+        double derivitive;
+        double start_time;
+        double ld_speed;
+        double rd_speed;
+        double straight;
+        double dt;
+        double PID;
+        //setting straight value
+        straight = 600;
+        //setting drive speed
+        ld_speed = 0.5;
+        rd_speed = 0.5;
+        //how often to run the loop
+        dt = 200;
+        //coefficients for PID loop
+        kp = 1;
+        ki = 2;
+        kd = 3;
+        //start time to compare against
+        start_time = java.util.Date.getTime();
+        //setting variables to zero to use in first loop round
+        intergral = 0;
+        previous_error = 0;
+        //driving initial values
+        leftMotor.setPower(ld_speed);
+        rightMotor.setPower(rd_speed);
+        switch (z){
+            case "d":
+                do {
+                    //error is the rotaion error
+                    error = straight - gyro.getRotation;
+                    //dividing errror because motor speed is in percentage
+                    error = error / 10;
+                    //proportional which is just error
+                    proportional = error;
+                    //intergral art of loop- error over time
+                    intergral = intergral + error * dt;
+                    //derivitive which uses slope to correct future error
+                    derivitive = (error - previous_error) / dt;
+                    //suming together to create complete correction value
+                    PID = kp * proportional + ki * intergral + kd * derivitive;
+                    //applying corrections to driving
+                    leftMotor.setPower(ld_speed + PID);
+                    rightMotor.setPower(rd_speed - PID)
+                    //setting vlues for next loop
+                    previous_error = error;
+                    ld_speed = ld_speed + PID;
+                    rd_speed = rd_speed - PID;
+                    //telemetry stuff
+                    telemetry.addData("pro", proportional);
+                    telemetry.addData("int", intergral);
+                    telemetry.addData("der", derivitive);
+                    telemetry.adddata("gyro", gyro.getRotation());
+                    telemetry.addData("error", error);
+                    sleep(dt);
+                } while (java.util.Date.getTime() - start_time < t);
+            case "t":
+                //add pis loop to use compass when turn is specified
+        }
+    }
 }
