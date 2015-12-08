a1c23487992c61bccd2b562d87789afc0abbf745
Drew Zoghby <Drew Zoghby>
2015-12-07 23:07:14 -0600

    added new thread to keep track of angle to keep trck of angle in parralel with the opmode

---
+++ GyroAngle.java
@@ -0,0 +1,9 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+//thread class to track anfgle using the run method in Gyro class
+public class GyroAngle {
+    public void track_angle(){
+        Gyro gyro = new Gyro();
+        Thread t = new Thread(gyro);
+        t.start();
+    }
+}
