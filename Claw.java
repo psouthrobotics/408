f3849b0b75e4f941036bf32639c5fb1c304af8ad
Drew Zoghby <Drew Zoghby>
2015-12-08 21:32:17 -0600

    added tank drive adaptations

--- Claw.java
+++ Claw.java
@@ -9,20 +9,13 @@ public class Claw extends OpMode {
     //drive motors
     DcMotor leftMotor;
     DcMotor rightMotor;
-    //rack and pinion motor
-    DcMotor pinion;
-    //claw roation
-    DcMotor claw;
+    DcMotor rightTank;
+    DcMotor leftTank;
     //compass
     CompassSensor compass;
     //drive values
     double left;
     double right;
-    double pinion_power;
-    double claw_power;
-    //drive values for claw
-    double leftM;
-    double rightM;
     //scale for expo
     double a;
 
@@ -36,13 +29,13 @@ public class Claw extends OpMode {
         //Mapping physical motors to variable
         leftMotor = hardwareMap.dcMotor.get("left_Motor");
         rightMotor = hardwareMap.dcMotor.get("right_Motor");
-        claw = hardwareMap.dcMotor.get("claw");
-        pinion = hardwareMap.dcMotor.get("rack");
+        leftTank = hardwareMap.dcMotor.get("left_tank");
+        rightTank = hardwareMap.dcMotor.get("right_tank");
         compass = hardwareMap.compassSensor.get("compass");
         //scale value
         a = 20;
         compass.setMode(CompassSensor.CompassMode.CALIBRATION_MODE);
-        rightMotor.setDirection(DcMotor.Direction.FORWARD );
+        leftMotor.setDirection(DcMotor.Direction.REVERSE );
 
     }
 
@@ -58,25 +51,14 @@ public class Claw extends OpMode {
         expoL();
         expoR();
         //set drive values for motors
-        leftMotor.setPower(-left);
+        leftMotor.setPower(left);
         rightMotor.setPower(right);
-
-        //pinion movement
-        pinion_power = -gamepad2.left_stick_y;
-        expoP();
-        pinion_power = Range.clip(pinion_power, -1, 1);
-        pinion.setPower(pinion_power);
-
-        claw_power = -gamepad2.right_stick_y;
-        claw_power = Range.clip(claw_power, -1, 1);
-        expoC();
-        claw.setPower(-gamepad2.right_stick_y);
+        leftTank.setPower(left - 0.02);
+        rightTank.setPower(right - 0.02);
 
         //telemetry
         telemetry.addData("Left Motor power", left);
 		telemetry.addData("Right Motor power", right);
-        telemetry.addData("rotation power", pinion_power);
-        telemetry.addData("Extension power", claw_power);
         telemetry.addData("compass calibration", compass.calibrationFailed());
         telemetry.addData("compass data", compass.getDirection());
     }
@@ -84,30 +66,17 @@ public class Claw extends OpMode {
     public void expoL() {
         double expo;
         double i;
-        i = leftM;
+        i = left;
         expo =i/((1+a)*(1-i));
-        leftM = expo;
+        left = expo;
     }
     //scale right drive value
     public void expoR() {
         double expo;
         double i;
-        i = rightM;
+        i = right;
         expo =i/((1+a)*(1-i));
-        rightM = expo;
+        right = expo;
     }
-    public void expoP() {
-        double expo;
-        double i;
-        i = pinion_power;
-        expo =i/((1+a)*(1-i));
-        pinion_power = expo;
-    }
-    public void expoC() {
-        double expo;
-        double i;
-        i = claw_power;
-        expo =i/((1+a)*(1-i));
-        claw_power = expo;
-    }
+
 }
