8763878eebd89b7c804866b662a9385c6496e8f9
Dean Schreiber <waduda3835@parkwayschools.net>
2016-02-04 17:24:00 -0600

    Updated straight autonomous for general purpose straight driving

--- Auto.java
+++ Auto.java
@@ -1,13 +1,12 @@
 package com.qualcomm.ftcrobotcontroller.opmodes;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotor;
 
 /**
  * Created by robotics on 12/8/15.
  */
 public class Auto extends Drive {
     public void runOpMode() throws InterruptedException {
+
         waitForStart();
         while(opModeIsActive()){
             go(100000, 0);
