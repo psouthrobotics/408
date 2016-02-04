fed5a320660cf4a2acf19d04abe4948af90b86c0
Dean Schreiber <waduda3835@parkwayschools.net>
2016-02-04 17:19:08 -0600

    Added Variable speed

--- Tank.java
+++ Tank.java
@@ -3,6 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 import com.qualcomm.robotcore.util.Range;
 
 public class Tank extends OpMode {
@@ -12,12 +13,13 @@ public class Tank extends OpMode {
     DcMotor rightTank;
     DcMotor leftTank;
     Servo servo;
+    Servo flipper;
+    UltrasonicSensor front;
     //drive values
     double left;
     double right;
+    double a = 1;
     //scale for expo
-    double a;
-
 
 
 
@@ -31,13 +33,13 @@ public class Tank extends OpMode {
         leftTank = hardwareMap.dcMotor.get("left_tank");
         rightTank = hardwareMap.dcMotor.get("right_tank");
         servo = hardwareMap.servo.get("servo");
-        //scale value
-        a = 20;
+        flipper = hardwareMap.servo.get("flip");
+        front = hardwareMap.ultrasonicSensor.get("front");
+
         leftMotor.setDirection(DcMotor.Direction.REVERSE );
         rightTank.setDirection(DcMotor.Direction.REVERSE);
         leftTank.setDirection(DcMotor.Direction.REVERSE);
         rightMotor.setDirection(DcMotor.Direction.FORWARD);
-
 
     }
 
@@ -50,31 +52,53 @@ public class Tank extends OpMode {
         left = Range.clip(left, -1, 1);
         right = Range.clip(right, -1, 1);
         //scale drive values for easier controlling
-        //expoL();
-        //expoR();
+        expoL();
+        expoR();
+
+        if (gamepad2.x)
+            servo.setPosition(1);
+        if (gamepad2.b)
+            servo.setPosition(0);
+        if (gamepad2.y)
+            servo.setPosition(0.5);
+        if (gamepad2.dpad_down)
+            a = 0.3;
+        if (gamepad2.dpad_up)
+            a = 1;
+        if (gamepad2.right_bumper)
+            flipper.setPosition(0.8);
+        if (gamepad2.left_bumper)
+            flipper.setPosition(0.2);
         //set drive values for
+        left = left * a;
+        right = right * a;
         leftMotor.setPower(left);
         rightMotor.setPower(right);
-        leftTank.setPower(left/3);
-        rightTank.setPower(right/3);
-        if (gamepad1.a)
-            servo.setPosition(1);
-        if (gamepad1.b)
-            servo.setPosition(0);
+        leftTank.setPower(left);
+        rightTank.setPower(right);
 
 
-        //telemetryc vz
+
+        //telemetry
         telemetry.addData("Left Motor power", left);
 		telemetry.addData("Right Motor power", right);
         telemetry.addData("Servo Position", servo.getPosition());
+        telemetry.addData("Distance in Front", front.getUltrasonicLevel());
+        telemetry.addData("Speed", a);
     }
     //scale left drive value
     public void expoL() {
-        left = left*left;
+        if (left > 0)
+            left = left*left;
+        if (left <= 0)
+            left = -(left*left);
     }
     //scale right drive value
     public void expoR() {
-        right = right*right;
+        if (right > 0)
+            right = right*right;
+        if (right <= 0)
+            right = -(right*right);
     }
 
 }
